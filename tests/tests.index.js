3bd65f63394ed191b1b75019dbdbd20174a7a152
Robert Matthew Mulder <rmmulder@gmail.com>
2018-06-28 15:40:47 -0700

    save work in process

--- tests/tests.index.js
+++ tests/tests.index.js
@@ -1,5 +1,9 @@
 var testsContext = require.context('../src', true, /(^index)\.js$/);
+console.log('============================================================');
+console.log('testsContext:', testsContext.keys());
 testsContext.keys().forEach(testsContext);
 
 var srcContext = require.context('./', true, /\.js$/);
+console.log('============================================================');
+console.log('srcContext:', srcContext.keys());
 srcContext.keys().forEach(srcContext);
